be762a63110df63cd5eb5bf5c9f769d7df11fb33
alparslanc <alparslanc@WSTEMFA44-21.stu.net.fr.ch>
2024-04-24 08:39:54 +0200

    JavaDoc

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,7 +6,13 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
+   /**
+    * Vérifie si les plaques sont dans les normes.
+    * Le canton doit contenir deux caractère qui se trouve dans [A-Z].
+    * @param canton est une chaine de caractère qui décrit les initiales des cantons.
+    * @param numeroDePlaque est un numéro qui différencit les plaques.
+    * @return si la plaque correspond aux normes.
+    */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
         return false;
     }
